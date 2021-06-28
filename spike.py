40febfc09db7495ee70d09dcecfb36908a179e5e
Amanda <52638577+otooleam@users.noreply.github.com>
2021-06-27 23:44:19 -0500

    v1
    definitely buggy

--- spike.py
+++ spike.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 TOKEN = ''
 join_emote = '\N{White Heavy Check Mark}'
 battle_emote = '\N{Crossed Swords}'
+leave_emote = '\N{No Entry Sign}'
 
 bot = commands.Bot(command_prefix='!')
 
@@ -13,33 +14,82 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='raid')
-async def raid(ctx, raid_boss, time_remaining):
+async def raid(ctx, *args):
     print('raid command recieved')
     
-    embed=discord.Embed(title=f'{raid_boss} Raid', description=f'Time Remaining: {time_remaining}')
-    embed.add_field(name='Host', value=f'{ctx.message.author.nick}', inline=False)
+    embed=discord.Embed(title=f'{" ".join(args[:-1])} Raid', description=f'Time Remaining: {args[-1]}')
+    embed.add_field(name='Host', value=f'{ctx.message.author.mention}', inline=False)
+    embed.add_field(name='Participants', value='none', inline=False)
+    embed.set_footer(text=f'{join_emote} to join. {leave_emote} to leave.')
 
     message = await ctx.send(embed = embed)
 
     await message.add_reaction(join_emote)
     await message.add_reaction(battle_emote)
+    await message.add_reaction(leave_emote)
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    if (user == bot.user):
+    if user == bot.user:
         return
-    elif reaction.emoji == join_emote:
+    elif reaction.message.author != bot.user:
+        return
+    elif len(reaction.message.embeds) < 1:
+        return
+    else:
         embed = reaction.message.embeds[0]
-        
-        if any(field.name == 'Participants' for field in embed.fields):
-            embed.set_field_at(1, 'Participants', embed.fields[1].value += '\n{user.nick}', False)
-        else:
-            embed.add_field(name='Participants', value=f'{user.nick}', inline=False)
-        
-        await reaction.message.edit(embed = embed)
-    elif reaction.emoji == battle_emote:
-        embed = reaction.message.embeds[0]
-        
-        await reaction.message.edit(embed = embed)
+        host = embed.fields[0].value
+        participants_list = embed.fields[1].value.split()
 
+        if reaction.emoji == join_emote:
+            if user.mention == host:
+                #remove react
+                return
+            elif user.mention in participants_list:
+                return
+            else:
+                if 'none' in participants_list:
+                    participants_list.remove('none')
+                participants_list.append(user.mention)
+        elif reaction.emoji == battle_emote:
+            if user.mention != host:
+                #remove reaction
+                return
+            elif 'none' in participants_list:
+                return
+            else:
+                #remove reaction
+                raiders = participants_list[:5]
+                participants_list = participants_list[5:]
+                await reaction.message.channel.send(f'{" ".join(raiders)}: invites from {host} for a {embed.title} will be sent shortly')
+        elif reaction.emoji == leave_emote:
+            if user.mention == host:
+                #remove react
+                return
+            elif user.mention not in participants_list:
+                return
+            else:
+                participants_list.remove(user.mention)
+                if len(participants_list) == 0:
+                    participants_list.append('none')
+        embed.set_field_at(1, name='Participants', value='\n'.join(participants_list), inline=False)
+        await reaction.message.edit(embed = embed)
+        
 bot.run(TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
